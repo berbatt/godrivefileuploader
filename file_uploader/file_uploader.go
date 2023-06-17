08c7f6b96949e900dfa7a88a4a245e5eda110ccb
Berkay Bat <berkay.bat@gmail.com>
2023-06-17 12:43:43 +0300

    implement create folder

--- file_uploader/file_uploader.go
+++ file_uploader/file_uploader.go
@@ -3,18 +3,31 @@ package file_uploader
 import (
 	"bytes"
 	"context"
+	"github.com/pkg/errors"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
-	"io/ioutil"
-	"log"
 	"os"
 )
 
 const pathCredentialFile = "credentials.json"
 
+var uploader *FileUploader
+
+func GetUploader() (FileUploader, error) {
+	if uploader == nil {
+		u, err := NewDefaultDriveUploader()
+		if err != nil {
+			return nil, err
+		}
+		uploader = &u
+	}
+	return *uploader, nil
+}
+
 type FileUploader interface {
-	UploadFileFrom(path string) error
+	UploadFileFrom(input []byte, fileName string, parentID string) error
+	CreateFolder(folderName, parentFolderID string) (string, error)
 }
 
 type DriveUploader struct {
@@ -24,11 +37,11 @@ type DriveUploader struct {
 func NewDriveUploader(pathCredentialFile string) (FileUploader, error) {
 	b, err := os.ReadFile(pathCredentialFile)
 	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
+		return nil, errors.Wrap(err, "Unable to read client secret file")
 	}
-	config, err := google.ConfigFromJSON(b, drive.DriveScope)
+	config, err := google.ConfigFromJSON(b, drive.DriveFileScope)
 	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
+		return nil, errors.Wrap(err, "Unable to parse client secret file to config")
 	}
 	httpClient, err := getClientFromConfig(config)
 	if err != nil {
@@ -45,18 +58,36 @@ func NewDefaultDriveUploader() (FileUploader, error) {
 	return NewDriveUploader(pathCredentialFile)
 }
 
-func (d *DriveUploader) UploadFileFrom(path string) error {
-	// Read the file contents
-	fileContents, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func (d *DriveUploader) UploadFileFrom(input []byte, fileName string, parentID string) error {
 	// Create a new file on Google Drive
-	file := &drive.File{Name: path}
-	_, err = d.client.Files.Create(file).Media(bytes.NewReader(fileContents)).Do()
+	file := &drive.File{
+		Name:    fileName,
+		Parents: []string{parentID},
+	}
+	_, err := d.client.Files.Create(file).Media(bytes.NewReader(input)).Do()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	return nil
+}
+
+// CreateFolder creates a folder with the given name in the specified parent folder (if any) and returns its ID.
+func (d *DriveUploader) CreateFolder(folderName, parentFolderID string) (string, error) {
+	var parents []string
+	if len(parentFolderID) > 0 {
+		parents = append(parents, parentFolderID)
+	}
+	folder := &drive.File{
+		Name:           folderName,
+		MimeType:       "application/vnd.google-apps.folder",
+		Parents:        parents,
+		FolderColorRgb: "#00FF00",
+	}
+
+	createdFolder, err := d.client.Files.Create(folder).Do()
+	if err != nil {
+		return "", errors.Wrapf(err, "unable to create folder with name: %s error: %v", folderName, err)
+	}
+
+	return createdFolder.Id, nil
 }

2688b9b49d541f70546bd840bf8d48a9443bc580
Berkay Bat <berkay.bat@gmail.com>
2023-06-18 22:13:39 +0300

    implement TokenStorage

---
+++ file_operations/token_storage.go
@@ -0,0 +1,77 @@
+package file_operations
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
+	"os"
+)
+
+const (
+	PathCredentialsFile = "credentials.json"
+	PathTokenFile       = "token.json"
+)
+
+type TokenStorage struct {
+	Token *oauth2.Token
+}
+
+func (ts *TokenStorage) SaveToken(token *oauth2.Token) error {
+	ts.Token = token
+	return storeToken(token)
+}
+
+func (ts *TokenStorage) LoadToken() (*oauth2.Token, error) {
+	token, err := loadToken()
+	if err != nil {
+		return nil, err
+	}
+	ts.Token = token
+	return ts.Token, nil
+}
+
+func loadToken() (*oauth2.Token, error) {
+	// Open the token file
+	file, err := os.Open(PathTokenFile)
+	if err != nil {
+		return nil, errors.Wrap(err, "token file doesn't exist or cannot be opened")
+	}
+	defer func(file *os.File) {
+		err = file.Close()
+		if err != nil {
+			return
+		}
+	}(file)
+
+	// Deserialize the token from JSON
+	token := &oauth2.Token{}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(token)
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to load token")
+	}
+
+	return token, nil
+}
+
+func storeToken(token *oauth2.Token) error {
+	// Create or open the token file
+	file, err := os.Create(PathTokenFile)
+	if err != nil {
+		return errors.Wrap(err, "Unable to create token file")
+	}
+	defer func(file *os.File) {
+		err = file.Close()
+		if err != nil {
+			return
+		}
+	}(file)
+
+	// Serialize the token to JSON
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(token)
+	if err != nil {
+		return errors.Wrap(err, "unable to store token")
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"github.com/pkg/errors"
-	"golang.org/x/oauth2"
+	"godrivefileuploader/file_operations"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
-	"log"
-	"net/http"
-	"os"
 )
 
 const (
@@ -25,123 +19,48 @@ func main() {
 			return
 		}
 	*/
-	err := authHelper()
-	if err != nil {
+
+	if err := handleAuthenticationFlow(); err != nil {
 		return
 	}
 }
 
-func authHelper() error {
-	b, err := os.ReadFile("credentials.json")
+func handleAuthenticationFlow() error {
+	credentialFile, err := file_operations.ReadFileFromPath(file_operations.PathCredentialsFile)
 	if err != nil {
 		return errors.Wrap(err, "Unable to read client secret file")
 	}
 
-	config, err := google.ConfigFromJSON(b, drive.DriveFileScope)
+	config, err := google.ConfigFromJSON(credentialFile, drive.DriveFileScope)
 	if err != nil {
 		return errors.Wrap(err, "Unable to parse client secret file to config")
 	}
-	// Create a token storage instance
-	//tokenStorage := &TokenStorage{}
 
-	// Load the token from storage
-	token := loadToken()
+	tokenStorage := file_operations.TokenStorage{}
+	token, err := tokenStorage.LoadToken()
+	if err != nil {
+		return err
+	}
+
+	driveAuthenticator := file_operations.DriveAuthenticator{
+		Token:  token,
+		Config: config,
+	}
 	if token != nil {
-		// Check if the token is still valid
-		if !token.Valid() {
-			// If the token is expired, refresh it
-			tokenSource := config.TokenSource(context.Background(), token)
-			newToken, err := tokenSource.Token()
-			if err != nil {
-				log.Fatalf("Unable to refresh token: %v", err)
-			}
-			token = newToken
-
-			// Save the refreshed token
-			storeToken(token)
+		if err = driveAuthenticator.RefreshToken(); err != nil {
+			return err
+		}
+		if err = tokenStorage.SaveToken(driveAuthenticator.Token); err != nil {
+			return err
+		}
+	} else if token == nil {
+		if err = driveAuthenticator.InitiateAuthenticationFlow(); err != nil {
+			return err
+		}
+		if err = tokenStorage.SaveToken(driveAuthenticator.Token); err != nil {
+			return err
 		}
 	}
 
-	// If no token is available, initiate the authorization flow
-	if token == nil {
-		// Start the authorization flow
-		url := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-		fmt.Printf("Go to the following URL to authorize the application: \n%v\n", url)
-
-		// Set up a simple HTTP server to handle the callback
-		http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-			code := r.URL.Query().Get("code")
-			if code == "" {
-				log.Fatal("Authorization code not found")
-			}
-
-			// Exchange the authorization code for a token
-			token, err := config.Exchange(context.Background(), code)
-			if err != nil {
-				log.Fatalf("Unable to exchange authorization code: %v", err)
-			}
-
-			// Save the token
-			storeToken(token)
-
-			fmt.Println("Authorization successful.")
-		})
-
-		// Start the HTTP server
-		log.Fatal(http.ListenAndServe(":8080", nil))
-	}
-
-	// Create an OAuth2 HTTP client
-	//httpClient := config.Client(context.Background(), token)
-
-	// Use the HTTP client to make authenticated API requests
-	// ...
 	return nil
-}
-
-type TokenStorage struct {
-	Token *oauth2.Token
-}
-
-func (ts *TokenStorage) SaveToken(token *oauth2.Token) {
-	ts.Token = token
-}
-
-func (ts *TokenStorage) LoadToken() *oauth2.Token {
-	return ts.Token
-}
-
-func storeToken(token *oauth2.Token) {
-	// Create or open the token file
-	file, err := os.Create("token.json")
-	if err != nil {
-		log.Fatalf("Unable to create token file: %v", err)
-	}
-	defer file.Close()
-
-	// Serialize the token to JSON
-	encoder := json.NewEncoder(file)
-	err = encoder.Encode(token)
-	if err != nil {
-		log.Fatalf("Unable to store token: %v", err)
-	}
-}
-
-func loadToken() *oauth2.Token {
-	// Open the token file
-	file, err := os.Open("token.json")
-	if err != nil {
-		return nil // Token file doesn't exist or cannot be opened
-	}
-	defer file.Close()
-
-	// Deserialize the token from JSON
-	token := &oauth2.Token{}
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(token)
-	if err != nil {
-		log.Fatalf("Unable to load token: %v", err)
-	}
-
-	return token
 }

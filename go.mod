5ea5b4eefb2af9e969b709335661b46a111232ed
Berkay Bat <berkay.bat@gmail.com>
2024-04-23 19:12:36 +0300

    take arguments from console

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module godrivefileuploader
 go 1.18
 
 require (
+	github.com/jessevdk/go-flags v1.5.0
 	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.8.4
 	golang.org/x/oauth2 v0.16.0
 	google.golang.org/api v0.156.0
 )
@@ -11,6 +13,7 @@ require (
 require (
 	cloud.google.com/go/compute v1.23.3 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -20,6 +23,7 @@ require (
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.46.1 // indirect
 	go.opentelemetry.io/otel v1.21.0 // indirect
@@ -33,4 +37,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240102182953-50ed04b92917 // indirect
 	google.golang.org/grpc v1.60.1 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,62 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"github.com/jessevdk/go-flags"
 	"godrivefileuploader/file_operations"
 	"log"
+	"os"
 )
 
 const (
-	pathname = "temp"
+	appName = "gofileuploader"
 )
 
+type exitCode int
+
+const (
+	exitCodeOK exitCode = iota
+	exitCodeErr
+)
+
+type options struct {
+	Period string `short:"P" long:"period" description:"Period of uploading files, such as '1h', '2m', '3d'. Default is 1 hour" default:"1h"`
+	Path   string `short:"p" long:"path" description:"Absolute path of the directory"`
+}
+
 func main() {
-	err := file_operations.TraverseThroughDirectoryAndUploadToDrive(pathname)
+	code, err := run(os.Args[1:])
 	if err != nil {
-		log.Fatalf("error while uploading the path with name: %s\nerr: %v", pathname, err)
-		return
+		_, err = fmt.Fprintln(os.Stderr, err)
+		if err != nil {
+			os.Exit(int(exitCodeErr))
+		}
 	}
+	os.Exit(int(code))
+}
+
+func run(args []string) (exitCode, error) {
+	var opts options
+	parser := flags.NewParser(&opts, flags.Default)
+	parser.Name = appName
+	parser.Usage = "[OPTIONS] QUERY..."
+	_, err := parser.ParseArgs(args)
+	if err != nil {
+		if flags.WroteHelp(err) {
+			return exitCodeOK, nil
+		}
+		return exitCodeErr, fmt.Errorf("argument parsing failed: %w", err)
+	}
+	if len(opts.Path) == 0 {
+		return exitCodeErr, errors.New("must enter a path")
+	}
+
+	err = file_operations.TraverseThroughDirectoryAndUploadToDrive(opts.Path)
+	if err != nil {
+		log.Fatalf("error while uploading the path with name: %s\nerr: %v", opts.Path, err)
+		return exitCodeErr, err
+	}
+
+	return exitCodeOK, nil
 }
